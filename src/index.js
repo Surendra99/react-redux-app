0fef9ebdb5f802670851ab47988d4121e5ee4717
siva <siva@sirpi.co>
2019-08-25 12:33:15 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+//store -> Globalized State
+
+//ACTION -> INCREAMENT
+const increament = () => {
+    return {
+        type:'INCREAMENT'
+    }
+}
+
+const decreament = ()=> {
+    return{
+        type:'DECREAMENT'
+    }
+}
+
+
+//REDUCER 
+const counter = (state=0,action) => {
+    switch(action.type){
+        case 'INCREAMENT':
+            return state+1;
+        case 'DECREAMENT':
+            return state-1;
+        default:
+            return state;    
+    }
+}
+const store = createStore(counter);
+
+store.subscribe(()=>console.log(store.getState()));
+//DISPATCH
+
+store.dispatch(increament());
+store.dispatch(decreament());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
