23ebdd04dcb76c39ad2c61b0d6b17d01f3afa5be
siva <siva@sirpi.co>
2019-08-26 16:12:46 +0000

    testing husky eslinting

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
   const counter = useSelector(state=>state.counter);
   const isLogged = useSelector(state=>state.isLogged);
   const dispatch = useDispatch();
+console.log('sdf');
 
-  return (
+return (
       <div>
         <h1>Counter:{counter}</h1>
         <button onClick={()=>dispatch(increament())}>+</button>
